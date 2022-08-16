ef46c697778275f923f9e882d216396e082a25cb
Andrey <yyootttaa@gmail.com>
2022-08-16 16:19:22 +0300

    Started working on Astar Points Stacker(APS)

---
+++ src/apf_la/src/astar_point_stacker.cpp
@@ -0,0 +1,28 @@
+// Create a node that takes point array from astar and stack them into a vector
+#include "ros/ros.h"
+#include "apf_la/GlobalTrajectory.h"
+
+apf_la::GlobalTrajectory trajectory_storage;
+
+void astar_cb(const apf_la::GlobalTrajectory::ConstPtr& trajectory)
+{
+    for (int i = 0; i < trajectory->waypoints.capacity(); i++)
+    {
+        // make a copy of the trajectory object in trajectory_storage
+        trajectory_storage.waypoints.push_back(trajectory->waypoints[i]);
+        trajectory_storage.waypoints.at(i).header.stamp = trajectory->waypoints.at(i).header.stamp;
+        trajectory_storage.waypoints.at(i).header.frame_id = trajectory->waypoints.at(i).header.frame_id;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "astar_point_stacker");
+    ros::NodeHandle n;
+
+    ros::Subscriber sub = n.subscribe<>("/astar/trajectory", 1000, astar_cb);
+    ros::Publisher pub = n.advertise<geometry_msgs::PoseStamped>("/point_stacker/actual_goal", 1000);
+
+    ros::spin();
+    return 0;
+}
